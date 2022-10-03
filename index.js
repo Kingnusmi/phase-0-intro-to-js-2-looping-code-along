f70d22779f0507cccf09d9e108c9ca14aafaa534
Kingnusmi <kingnusmi14@gmail.com>
2022-10-03 15:44:20 -0400

    commit tree 1f79670ffd5852f288dde8658d0a7edd16ebb94f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]; 
+
+function writeCards(names) {
+    let i = 0;
+    while (i < names.length) {
+        console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        i++
+        debugger
+    }
+    return names;
+}
+writeCards(names);
+
+let countDown = 10;
+    while (countDown > -1) {
+    console.log(countDown--);}
